2390eddb9c437b0bdfadb8cdd0fc6f291246519a
aishwarya25-dev <aishwaryaj2510@gmail.com>
2026-06-09 20:11:40 +0530

    star factory operational

---
+++ projects/Star_Pattern.py
@@ -0,0 +1,33 @@
+# Different Types of pattern.
+
+# Right Triangle
+print("Right Triangle :")
+for i in range(1, 5):
+    print(" * " * i)
+
+# Inverted Right Triangle
+print("Inverted Right Triangle :")
+for i in range(5, 0, -1):
+    print(" * " * i)
+
+# Square Pattern
+print("Square Pattern :")
+for i in range(5):
+    print(" * " * 5)
+
+    # Different Types of pattern.
+
+# Right Triangle
+print("Right Triangle :")
+for i in range(1, 5):
+    print(" * " * i)
+
+# Inverted Right Triangle
+print("Inverted Right Triangle :")
+for i in range(5, 0, -1):
+    print(" * " * i)
+
+# Square Pattern
+print("Square Pattern :")
+for i in range(5):
+    print(" * " * 5)
